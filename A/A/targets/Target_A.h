a604b1171c78675b2a100d69492bb897d77b1f6b
guobangbang <guobangbang@syswin.com>
2019-05-24 11:29:03 +0800

    2

---
+++ A/A/targets/Target_A.h
@@ -0,0 +1,15 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by guobangbang on 2019/5/22.
+//  Copyright © 2019 GBZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
+@end
---
+++ A/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  A
+//
+//  Created by guobangbang on 2019/5/24.
+//  Copyright © 2019 GBZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
